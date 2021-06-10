6d699ccabeba61e620034e722b6336fdceafaf95
Code555 <85639978+Ika555@users.noreply.github.com>
2021-06-10 00:04:19 +0000

    Delete Hello

--- hello.js
+++ hello.js
@@ -1,8 +1,2 @@
-function hello() {
-	// TODO: change this string so that your program prints "Hello world!" 
-	// when you hit the replit run button
-	return "Hello!";
-}
- 
-module.exports = hello;
+
 console.log("Hello World!");
